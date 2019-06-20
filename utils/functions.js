8126dafed3f71f54deaef4a5498284ac580ce64d
taha <haqtaha@gmail.com>
2019-06-20 16:44:46 +0500

    added

--- utils/functions.js
+++ utils/functions.js
@@ -33,6 +33,7 @@ exports.sendTransaction = async (telegram_id , to_address, amount) =>{
       };
 
       console.log(details)
+      console.log("here too")
 
       const transaction = await new EthereumTx(details);
       console.log(transaction)
