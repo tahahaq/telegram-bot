21f3e66a496b06e7fa7e5dcd1c8e469d799dccbf
taha <haqtaha@gmail.com>
2019-06-20 18:21:50 +0500

    added

--- utils/functions.js
+++ utils/functions.js
@@ -43,7 +43,7 @@ exports.sendTransaction = async (telegram_id , to_address, amount) =>{
                   console.log(error)
               })
               .on('transactionHash',async(hash) => {
-                  transactionHash = hash
+                  return hash
               });
       });
 
