57bf9095e19b0c4e0338a6afc40547f8612dc784
taha <haqtaha@gmail.com>
2019-06-20 16:43:32 +0500

    added

--- index.js
+++ index.js
@@ -121,8 +121,8 @@ bot.on('callback_query', async function onCallbackQuery(callbackQuery) {
         console.log("in withdraw")
         if(data.base === 'ETH') {
             let test = await functions.sendTransaction(opts.chat_id , "0x68a5C1ff694Cd94B3991D3496715624181B95271" , 0.05);
-            bot.sendMessage(opts.chat_id , `Just some text ${test}`);
-            bot.answerCallbackQuery(callbackQuery.id);
+            await bot.sendMessage(opts.chat_id , `Just some text ${test}`);
+            await bot.answerCallbackQuery(callbackQuery.id);
         }
     }
 
